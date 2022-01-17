5a8fc22612935d4bb05255c30559fbf2f301cd99
RITVIK G <ritviksrinivas@gmail.com>
2022-01-17 14:43:41 +0530

    Create insertRecord.sql

---
+++ DBMS/Lab3/insertRecord.sql
@@ -0,0 +1,10 @@
+// Inserting records from PDF
+
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('001', 'Monika', 'Arora', '100000', '2014-02-20 09:00:00', 'HR');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('002', 'Niharika', 'Verma', '80000', '2014-06-11 09:00:00', 'Admin');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('003', 'Vishal', 'Singhal', '300000', '2014-02-20 09:00:00', 'HR');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('004', 'Amitabh', 'Singh', '500000', '2014-02-20 09:00:00', 'Admin');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('005', 'Vivek', 'Bhati', '500000', '2014-06-11 09:00:00', 'Admin');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('006', 'Vipul', 'Diwan', '200000', '2014-06-11 09:00:00', 'Account');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('007', 'Satish', 'Kumar', '75000', '2014-01-20 09:00:00', 'Account');
+INSERT INTO `test`.`worker` (`WORKER_ID`, `FIRST_NAME`, `LAST_NAME`, `SALARY`, `JOINING_DATE`, `DEPARTMENT`) VALUES ('008', 'Geetika', 'Chauhan', '90000', '2014-04-11 09:00:00', 'Admin');
